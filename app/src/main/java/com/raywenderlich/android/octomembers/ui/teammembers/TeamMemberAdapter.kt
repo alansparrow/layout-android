36392a8f5c5c94adba8e7b18a934d120305d8b90
Fuzzy Rock <baotrungtn@gmail.com>
2018-08-24 03:45:59 +0900

    Material Design

--- app/src/main/java/com/raywenderlich/android/octomembers/ui/teammembers/TeamMemberAdapter.kt
+++ app/src/main/java/com/raywenderlich/android/octomembers/ui/teammembers/TeamMemberAdapter.kt
@@ -37,6 +37,7 @@ import android.view.ViewGroup
 import com.raywenderlich.android.octomembers.R
 import com.raywenderlich.android.octomembers.model.Member
 import com.raywenderlich.android.octomembers.ui.member.MemberActivity
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.list_item_team_member.view.*
 
 class TeamMemberAdapter(var members: List<Member>) : RecyclerView.Adapter<TeamMemberAdapter.TeamMemberViewHolder>() {
@@ -60,7 +61,10 @@ class TeamMemberAdapter(var members: List<Member>) : RecyclerView.Adapter<TeamMe
 
     fun bind(member: Member) {
       this.member = member
+      Picasso.with(itemView.teamMemberAvatar.context).load(member.avatarUrl)
+              .into(itemView.teamMemberAvatar)
       itemView.teamMemberLogin.text = member.login
+      itemView.teamMemberType.text = member.type
     }
 
     override fun onClick(view: View) {
